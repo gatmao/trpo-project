d59041cd06f978af2dc54061b9975326cdb1cea3
Kris Tyum <gatmao@gmail.com>
2017-06-14 03:18:22 +0700

    app base, serve static, bootstrap

---
+++ photo_trade/urls.py
@@ -0,0 +1,26 @@
+"""photo_trade URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/1.11/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.conf.urls import url, include
+    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
+"""
+from django.conf.urls import url
+from django.contrib import admin
+from django.conf.urls.static import static
+from django.views.generic import TemplateView
+
+from photo_trade.settings import STATIC_URL, STATIC_ROOT
+
+urlpatterns = [
+    url(r'^$', TemplateView.as_view(template_name='layouts/main.html')),
+    url(r'^admin/', admin.site.urls),
+] + static(STATIC_URL, document_root=STATIC_ROOT)
